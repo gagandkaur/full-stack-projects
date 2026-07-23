92ca9560d8d23197f9f0f911a253172b09b3e6a5
Gagandeep Kaur <kaurgagandeep2316@gmail.com>
2026-07-24 00:01:43 +0530

    Clean up comments in vite.config.js
    Removed comments explaining the Vite configuration.

--- exp 1/vite.config.js
+++ exp 1/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Standard Vite config for a React project.
-// This just tells Vite to use the React plugin.
+
 export default defineConfig({
   plugins: [react()],
 });
